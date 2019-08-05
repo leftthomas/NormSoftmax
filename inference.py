c245b339c11397af3d7b6bc32f80a972f10f84ba
leftthomas <leftthomas@qq.com>
2019-08-05 18:56:27 +0800

    add inference

--- inference.py
+++ inference.py
@@ -1,14 +1,12 @@
 import argparse
+import os
 
 import torch
-from PIL import Image
-from torchvision import transforms
-
-from utils import rgb_mean, rgb_std
+from PIL import Image, ImageDraw
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Image Retrieval Demo')
-    parser.add_argument('--query_img_name', default='data/car/uncropped/000001.jpg', type=str, help='query image name')
+    parser.add_argument('--query_img_name', default='data/car/uncropped/008055.jpg', type=str, help='query image name')
     parser.add_argument('--data_base', default='car_uncropped_resnet18_48_12_data_base.pth', type=str,
                         help='queried database')
     parser.add_argument('--retrieval_num', default=8, type=int, help='retrieval number')
@@ -17,37 +15,43 @@ if __name__ == '__main__':
 
     QUERY_IMG_NAME, DATA_BASE, RETRIEVAL_NUM = opt.query_img_name, opt.data_base, opt.retrieval_num
     DATA_NAME = DATA_BASE.split('_')[0]
-    normalize = transforms.Normalize(rgb_mean[DATA_NAME], rgb_std[DATA_NAME])
-    transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor(), normalize])
 
-    query_data_base = torch.load('results/{}'.format(DATA_BASE))
-    if QUERY_IMG_NAME not in query_data_base['test_images']:
+    data_base = torch.load('results/{}'.format(DATA_BASE))
+    if QUERY_IMG_NAME not in data_base['test_images']:
         raise FileNotFoundError('{} not found'.format(QUERY_IMG_NAME))
-    query_image = transform(Image.open(QUERY_IMG_NAME).convert('RGB'))
-    query_label = query_data_base['test_labels'][query_data_base['test_images'].index(QUERY_IMG_NAME)]
-    query_feature = query_data_base['test_features'][query_data_base['test_images'].index(QUERY_IMG_NAME)]
+    query_index = data_base['test_images'].index(QUERY_IMG_NAME)
+    query_image = Image.open(QUERY_IMG_NAME).convert('RGB').resize((256, 256), resample=Image.BILINEAR)
+    query_label = data_base['test_labels'][query_index]
+    query_feature = data_base['test_features'][query_index]
 
-    gallery_images = query_data_base['gallery_images']
-    gallery_labels = query_data_base['gallery_labels']
-    gallery_features = query_data_base['gallery_features']
+    gallery_images = data_base['gallery_images']
+    gallery_labels = data_base['gallery_labels']
+    gallery_features = data_base['gallery_features']
 
-    num_features = len(feature_labels)
-    feature_labels = torch.tensor(feature_labels)
-    feature_vectors = feature_vectors.permute(1, 0, 2).contiguous()
-    if gallery_vectors is None:
-        gallery_vectors = feature_vectors.permute(0, 2, 1).contiguous()
-    else:
-        gallery_vectors = gallery_vectors.permute(1, 2, 0).contiguous()
-    sim_matrix = feature_vectors.bmm(gallery_vectors)
-    sim_matrix = torch.mean(sim_matrix, dim=0)
-    if gallery_labels is None:
-        sim_matrix[torch.eye(num_features).byte()] = -1
-        gallery_labels = feature_labels
-    else:
-        gallery_labels = torch.tensor(gallery_labels)
+    query_label = torch.tensor(query_label)
+    query_feature = query_feature.view(1, *query_feature.size()).permute(1, 0, 2).contiguous()
+    gallery_labels = torch.tensor(gallery_labels)
+    gallery_features = gallery_features.permute(1, 2, 0).contiguous()
 
+    sim_matrix = query_feature.bmm(gallery_features).mean(dim=0).squeeze(dim=0)
+    if DATA_NAME is not 'isc':
+        sim_matrix[query_index] = -1
     idx = sim_matrix.argsort(dim=-1, descending=True)
-    acc_list = []
-    for r in rank:
-        correct = (gallery_labels[idx[:, 0:r]] == feature_labels.unsqueeze(dim=-1)).any(dim=-1).float()
-        acc_list.append((torch.sum(correct) / num_features).item())
+
+    result_path = 'results/{}'.format(QUERY_IMG_NAME.split('/')[-1].split('.')[0])
+    if not os.path.exists(result_path):
+        os.mkdir(result_path)
+    query_image.save('{}/query_img.jpg'.format(result_path))
+    for num, index in enumerate(idx[:RETRIEVAL_NUM]):
+        retrieval_image = Image.open(gallery_images[index.item()]).convert('RGB') \
+            .resize((256, 256), resample=Image.BILINEAR)
+        draw = ImageDraw.Draw(retrieval_image)
+        retrieval_label = gallery_labels[index.item()]
+        retrieval_status = (retrieval_label == query_label).item()
+        retrieval_prob = sim_matrix[index.item()].item()
+        if retrieval_status:
+            draw.rectangle((0, 0, 255, 255), outline='green', width=5)
+        else:
+            draw.rectangle((0, 0, 255, 255), outline='red', width=5)
+        draw.text((10, 10), '%.4f' % retrieval_prob, fill=(255, 0, 0))
+        retrieval_image.save('{}/retrieval_img_{}.jpg'.format(result_path, num + 1))
