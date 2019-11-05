2f16b4679e6850cde5a760dbfc8b0aa7e841a70a
leftthomas <leftthomas@qq.com>
2019-11-06 02:18:41 +0800

    change epochs to 20

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
                         choices=['maxpool', 'layer1', 'layer2', 'layer3', 'layer4'], help='shared module type')
     parser.add_argument('--with_random', action='store_true', help='with branch random weight or not')
     parser.add_argument('--batch_size', default=8, type=int, help='train batch size')
-    parser.add_argument('--num_epochs', default=12, type=int, help='train epoch number')
+    parser.add_argument('--num_epochs', default=20, type=int, help='train epoch number')
     parser.add_argument('--ensemble_size', default=48, type=int, help='ensemble model size')
     parser.add_argument('--meta_class_size', default=12, type=int, help='meta class size')
     parser.add_argument('--gpu_ids', default='0,1', type=str, help='selected gpu')
