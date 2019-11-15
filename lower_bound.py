c350d5c42d053111da68cb8e20ce1e129e135ae2
leftthomas <leftthomas@qq.com>
2019-11-15 16:25:14 +0800

    fix bound bug

--- lower_bound.py
+++ lower_bound.py
@@ -41,7 +41,7 @@ def get_index_m_lower_bound(n, s, epsilon=1e-8):
 
 X_s_list, X_m_list, Y_s_list, Y_m_list, Z_s_list, Z_m_list, Z_s_index_list, Z_m_index_list = [], [], [], [], [], [], [], []
 for i in range(1000, 20000, 500):
-    for j in range(2, 100, 5):
+    for j in range(2, 1000, 5):
         X_s_list.append(i)
         Y_s_list.append(j)
         Z_s_list.append((s_lower_bound(i, j)))
@@ -59,12 +59,12 @@ Z_s_list, Z_m_list = np.array(Z_s_list), np.array(Z_m_list)
 fig = plt.figure()
 ax = Axes3D(fig)
 
-ax.scatter3D(X_m_list, Y_m_list, Z_m_list)
-ax.set_xlabel('n value')
-ax.set_ylabel('k value')
-ax.set_zlabel('m value')
-plt.savefig('m_value.pdf')
-plt.show()
+# ax.scatter3D(X_m_list, Y_m_list, Z_m_list)
+# ax.set_xlabel('n value')
+# ax.set_ylabel('k value')
+# ax.set_zlabel('m value')
+# plt.savefig('m_value.pdf')
+# plt.show()
 
 # ax.scatter3D(X_s_list, Y_s_list, Z_s_list)
 # ax.set_xlabel('n value')
@@ -80,9 +80,9 @@ plt.show()
 # plt.savefig('m_index.pdf')
 # plt.show()
 
-# ax.scatter3D(X_s_list, Y_s_list, Z_s_index_list)
-# ax.set_xlabel('n value')
-# ax.set_ylabel('m value')
-# ax.set_zlabel('constraint number')
-# plt.savefig('k_index.pdf')
-# plt.show()
+ax.scatter3D(X_s_list, Y_s_list, Z_s_index_list)
+ax.set_xlabel('n value')
+ax.set_ylabel('m value')
+ax.set_zlabel('constraint number')
+plt.savefig('k_index.pdf')
+plt.show()
