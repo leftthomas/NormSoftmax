a7c9962758c7b1dfa731c9e32ec120fd132dc6a6
leftthomas <leftthomas@qq.com>
2019-11-16 08:41:39 +0800

    update plot of k and m

--- plot_k.py
+++ plot_k.py
@@ -2,22 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # for k
-# -0.001923 x^2 + 0.154 x + 62.47
+# -2.895e-06 x^4 + 0.0005505 x^3 - 0.03675 x^2 + 0.9706 x + 57.26
 x = np.array([6, 10, 12, 18, 24, 36, 48, 56, 60, 80])
 y = np.array([61.6, 63.8, 65.3, 65.2, 65.9, 65.5, 65.0, 64.1, 64.8, 63.0])
 
-z1 = np.polyfit(x, y, 2)  # 用2次多项式拟合
-p1 = np.poly1d(z1)
-print(p1)  # 在屏幕上打印拟合多项式
-plot1 = plt.plot(x, y, '*', label='original values')
+z = np.polyfit(x, y, 4)  # using 4 polynomial to fit
+p = np.poly1d(z)
+print(p)
+plot1 = plt.plot(x, y, '*', label='real recall@1')
 
 x2 = np.linspace(1, 100, 100)
-yvals = p1(x2)
-plot2 = plt.plot(x2, yvals, 'r', label='polyfit values')
+y2 = p(x2)
+plot2 = plt.plot(x2, y2, 'r', label='predict recall@1')
 
-plt.xlabel('x axis')
-plt.ylabel('y axis')
-plt.legend(loc=4)  # 指定legend的位置,读者可以自己help它的用法
-plt.title('polyfitting')
-plt.savefig('k_value.pdf')
+plt.xlabel('k value')
+plt.ylabel('recall@1')
+plt.legend(loc=4)
+plt.title('k ~ recall@1')
+plt.savefig('k_recall.pdf')
 plt.show()
