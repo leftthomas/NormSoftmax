8d8c4d02c7694d21d106f6486e46dc1268d9d96d
leftthomas <leftthomas@qq.com>
2019-11-20 15:38:49 +0800

    add plot_surface

--- plot_prob.py
+++ plot_prob.py
@@ -1,16 +1,6 @@
-import math
-
 import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-
-def get_prob(n, k):
-    assert 1 < k < n
-    a, b = math.floor(n / k), math.ceil(n / k)
-    i = k - (n % k)
-    return (i * a * (a - 1) + (k - i) * b * (b - 1)) / (n * (n - 1))
-
 
 N, K, Prob = [], [], []
 for n in range(10, 30, 1):
@@ -18,14 +8,17 @@ for n in range(10, 30, 1):
         if n > k:
             N.append(n)
             K.append(k)
-            Prob.append((get_prob(n, k)))
 
-N, K, Prob = np.array(N), np.array(K), np.array(Prob)
+N, K = np.array(N), np.array(K)
+N, K = np.meshgrid(N, K)
+a, b = np.floor(N / K), np.ceil(N / K)
+i = K - (N % K)
+Prob = (i * a * (a - 1) + (K - i) * b * (b - 1)) / (N * (N - 1))
 
 fig = plt.figure()
 ax = Axes3D(fig, azim=-225)
 
-ax.scatter3D(N, K, Prob)
+ax.plot_surface(N, K, Prob, cmap='rainbow')
 ax.set_xlabel('n value')
 ax.set_ylabel('k value')
 ax.set_zlabel('prob of invalid')
