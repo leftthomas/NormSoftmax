6d86e054f96d06e951e15f213a2f2681ce6a08c5
leftthomas <leftthomas@qq.com>
2020-03-05 16:04:58 +0800

    simplified code

--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
-import numpy as np
 import torch
 from PIL import Image
-from torch import nn
-from torch.nn import functional as F
 from torch.utils.data import Dataset
-from torch.utils.data.sampler import Sampler
 from torchvision import transforms
 
 
@@ -56,68 +52,3 @@ def recall(feature_vectors, feature_labels, rank, gallery_vectors=None, gallery_
         correct = (gallery_labels[idx[:, 0:r]] == feature_labels.unsqueeze(dim=-1)).any(dim=-1).float()
         acc_list.append((torch.sum(correct) / num_features).item())
     return acc_list
-
-
-class BatchHardTripletLoss(nn.Module):
-    def __init__(self, margin=1.0):
-        super().__init__()
-        self.margin = margin
-
-    @staticmethod
-    def get_anchor_positive_triplet_mask(target):
-        mask = torch.eq(target.unsqueeze(0), target.unsqueeze(1))
-        mask.fill_diagonal_(False)
-        return mask
-
-    @staticmethod
-    def get_anchor_negative_triplet_mask(target):
-        labels_equal = torch.eq(target.unsqueeze(0), target.unsqueeze(1))
-        mask = ~ labels_equal
-        return mask
-
-    def forward(self, x, target):
-        pairwise_dist = torch.cdist(x.unsqueeze(0), x.unsqueeze(0)).squeeze(0)
-
-        mask_anchor_positive = self.get_anchor_positive_triplet_mask(target)
-        anchor_positive_dist = mask_anchor_positive.float() * pairwise_dist
-        hardest_positive_dist = anchor_positive_dist.max(1, True)[0]
-
-        mask_anchor_negative = self.get_anchor_negative_triplet_mask(target)
-        # make positive and anchor to be exclusive through maximizing the dist
-        max_anchor_negative_dist = pairwise_dist.max(1, True)[0]
-        anchor_negative_dist = pairwise_dist + max_anchor_negative_dist * (1.0 - mask_anchor_negative.float())
-        hardest_negative_dist = anchor_negative_dist.min(1, True)[0]
-
-        loss = (F.relu(hardest_positive_dist - hardest_negative_dist + self.margin))
-        return loss.mean()
-
-
-class MPerClassSampler(Sampler):
-    def __init__(self, labels, batch_size, m=4):
-        self.labels = np.array(labels)
-        self.labels_unique = np.unique(labels)
-        self.batch_size = batch_size
-        self.m = m
-        assert batch_size % m == 0, 'batch size must be divided by m'
-
-    def __len__(self):
-        return len(self.labels) // self.batch_size
-
-    def __iter__(self):
-        for _ in range(self.__len__()):
-            labels_in_batch = set()
-            inds = np.array([], dtype=np.int)
-
-            while inds.shape[0] < self.batch_size:
-                sample_label = np.random.choice(self.labels_unique)
-                if sample_label in labels_in_batch:
-                    continue
-
-                labels_in_batch.add(sample_label)
-                sample_label_ids = np.argwhere(np.in1d(self.labels, sample_label)).reshape(-1)
-                subsample = np.random.permutation(sample_label_ids)[:self.m]
-                inds = np.append(inds, subsample)
-
-            inds = inds[:self.batch_size]
-            inds = np.random.permutation(inds)
-            yield list(inds)
