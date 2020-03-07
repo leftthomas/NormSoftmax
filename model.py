b6fdfc109ae2fc6270948b689ee87f2254337c12
leftthomas <leftthomas@qq.com>
2020-03-08 04:15:58 +0800

    add k_means pooling

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch
+from capsule_layer.functional import k_means_routing
 from torch import nn
 from torch.nn import functional as F
 
@@ -11,8 +12,34 @@ def set_bn_eval(m):
         m.eval()
 
 
+class Pooling(nn.Module):
+    def __init__(self, pooling_mode='k_means', splits=8):
+        super().__init__()
+        assert pooling_mode in ['k_means', 'map', 'avp'], 'pooling_mode {} is not supported'.format(pooling_mode)
+        self.pooling_mode = pooling_mode
+        self.splits = splits
+
+    def forward(self, x):
+        assert x.dim() == 4, 'the input tensor must be the shape of [B, C, H, W]'
+        if self.pooling_mode == 'map':
+            return torch.flatten(F.adaptive_max_pool2d(x, output_size=(1, 1)), start_dim=1)
+        elif self.pooling_mode == 'avp':
+            return torch.flatten(F.adaptive_avg_pool2d(x, output_size=(1, 1)), start_dim=1)
+        else:
+            b, c, h, w = x.size()
+            assert c % self.splits == 0, 'the channel of input tensor must be divided by {}'.format(self.splits)
+            x = x.view(b, self.splits, c // self.splits, h * w)
+            x = x.permute(0, 1, 3, 2).contiguous()
+            y, _ = k_means_routing(x, num_iterations=3, similarity='cosine')
+            y = y.view(b, c)
+            return y
+
+    def extra_repr(self):
+        return 'pooling_mode={}'.format(self.pooling_mode)
+
+
 class Model(nn.Module):
-    def __init__(self, backbone_type, feature_dim, num_classes):
+    def __init__(self, backbone_type, feature_dim, num_classes, pooling_mode='k_means', splits=8):
         super().__init__()
 
         # Backbone Network
@@ -26,13 +53,14 @@ class Model(nn.Module):
         self.features = nn.Sequential(*self.features)
 
         # Refactor Layer
+        self.pooling = Pooling(pooling_mode, splits)
         self.refactor = nn.Linear(512 * expansion, feature_dim, bias=False)
         # Classification Layer
         self.fc = nn.Sequential(nn.BatchNorm1d(feature_dim), nn.Linear(feature_dim, num_classes, bias=False))
 
     def forward(self, x):
-        global_feature = F.adaptive_max_pool2d(self.features(x), output_size=(1, 1))
-        global_feature = torch.flatten(global_feature, start_dim=1)
+        global_feature = self.features(x)
+        global_feature = self.pooling(global_feature)
         feature = self.refactor(global_feature)
         classes = self.fc(feature)
         return F.normalize(feature, dim=-1), classes
