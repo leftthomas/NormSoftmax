b867a449b64e9e0ee252bf90568f81fe08703533
leftthomas <leftthomas@qq.com>
2020-03-08 12:36:05 +0800

    simplified code

--- resnet.py
+++ resnet.py
@@ -1,6 +1,5 @@
-import torch
 import torch.nn as nn
-from torchvision.models.resnet import conv1x1, conv3x3, BasicBlock, Bottleneck
+from torchvision.models.resnet import conv1x1, conv3x3, BasicBlock, Bottleneck, ResNet
 from torchvision.models.utils import load_state_dict_from_url
 
 model_urls = {
@@ -30,20 +29,13 @@ class SEModule(nn.Module):
         return identity * x
 
 
-class SEBottleneck(nn.Module):
-    expansion = 4
+class SEBottleneck(Bottleneck):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(SEBottleneck, self).__init__()
+        super(SEBottleneck, self).__init__(inplanes, planes, stride, downsample)
         self.conv1 = conv1x1(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = conv1x1(planes, planes * self.expansion)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
         self.se_module = SEModule(planes * self.expansion)
-        self.downsample = downsample
 
     def forward(self, x):
         identity = x
@@ -67,64 +59,18 @@ class SEBottleneck(nn.Module):
         return out
 
 
-class ResNet(nn.Module):
+class EResNet(ResNet):
 
     def __init__(self, block, layers, num_classes=1000, ceil_mode=False):
-        super(ResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
+        super(EResNet, self).__init__(block, layers, num_classes)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=ceil_mode)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         # remove down sample for stage3
+        self.inplanes = 512
         self.layer3 = self._make_layer(block, 256, layers[2], stride=1)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                nn.BatchNorm2d(planes * block.expansion))
-
-        layers = [block(self.inplanes, planes, stride, downsample)]
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-
-        return x
 
 
 def _resnet(arch, block, layers, pretrained, progress, **kwargs):
-    model = ResNet(block, layers, **kwargs)
+    model = EResNet(block, layers, **kwargs)
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls[arch], progress=progress, map_location='cpu')
         if arch == 'seresnet50':
