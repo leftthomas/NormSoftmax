f5ec23a7b081ead5f445ef863072d5d0dfb12b53
leftthomas <leftthomas@qq.com>
2020-03-12 17:58:38 +0800

    refactor model

--- model.py
+++ model.py
@@ -13,31 +13,6 @@ def set_bn_eval(m):
         m.eval()
 
 
-class Refactor(nn.Module):
-    def __init__(self, in_features, out_features, refactor_mode='tra'):
-        super(Refactor, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.refactor_mode = refactor_mode
-        self.fc = nn.Linear(in_features, out_features, bias=False)
-
-    def forward(self, x):
-        if self.refactor_mode == 'tra':
-            x = torch.flatten(F.adaptive_avg_pool2d(x, output_size=(1, 1)), start_dim=1)
-        elif self.refactor_mode == 'max':
-            x = torch.flatten(F.adaptive_max_pool2d(x, output_size=(1, 1)), start_dim=1)
-        elif self.refactor_mode == 'avg':
-            x = torch.flatten(F.adaptive_avg_pool2d(x, output_size=(1, 1)), start_dim=1)
-        else:
-            sum_value = x.pow(3).mean(dim=[-1, -2])
-            x = torch.sign(sum_value) * (torch.abs(sum_value).pow(1.0 / 3))
-        output = self.fc(x)
-        return output
-
-    def extra_repr(self):
-        return 'in_features={}, out_features={}'.format(self.in_features, self.out_features)
-
-
 class ProxyLinear(nn.Module):
     def __init__(self, in_features, out_features):
         super(ProxyLinear, self).__init__()
@@ -55,26 +30,34 @@ class ProxyLinear(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, backbone_type, feature_dim, num_classes, refactor_mode='tra'):
+    def __init__(self, backbone_type, feature_dim, num_classes):
         super().__init__()
 
         # Backbone Network
         backbones = {'resnet18': (resnet18, 1), 'resnet50': (resnet50, 4), 'seresnet50': (seresnet50, 4)}
         backbone, expansion = backbones[backbone_type]
-        self.features = []
+        self.layer0 = []
         for name, module in backbone(pretrained=True).named_children():
             if isinstance(module, nn.AdaptiveAvgPool2d) or isinstance(module, nn.Linear):
                 continue
-            self.features.append(module)
-        self.features = nn.Sequential(*self.features)
+            if name not in ['layer1', 'layer2', 'layer3', 'layer4']:
+                self.layer0.append(module)
+            else:
+                self.add_module(name, module)
+        self.layer0 = nn.Sequential(*self.layer0)
 
         # Refactor Layer
-        self.refactor = Refactor(512 * expansion, feature_dim, refactor_mode)
+        self.refactor = nn.Linear(512 * expansion, feature_dim, bias=False)
         # Classification Layer
         self.fc = nn.Sequential(nn.BatchNorm1d(feature_dim), ProxyLinear(feature_dim, num_classes))
 
     def forward(self, x):
-        global_feature = self.features(x)
-        feature = self.refactor(global_feature)
+        res0 = self.layer0(x)
+        res1 = self.layer1(res0)
+        res2 = self.layer2(res1)
+        res3 = self.layer3(res2)
+        res4 = self.layer4(res3)
+        feature = self.refactor(res4)
+        feature = torch.flatten(F.adaptive_max_pool2d(feature, output_size=(1, 1)), start_dim=1)
         classes = self.fc(feature)
         return F.normalize(feature, dim=-1), classes
